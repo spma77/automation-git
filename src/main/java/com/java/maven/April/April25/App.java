78a4759ce5bdc7bc9c5471b409bc74f6dff57172
suryaprabha <suryaprabha.v@gmail.com>
2017-04-26 22:27:31 -0700

    code

---
+++ src/main/java/com/java/maven/April/April25/App.java
@@ -0,0 +1,13 @@
+package com.java.maven.April.April25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
